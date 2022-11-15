aff48ede8cd866cd9fb1df6f9fc8034b0df6abd9
Mark Ruddy <1markruddy@gmail.com>
2022-11-15 19:27:51 +0000

    add student to interface

--- IStudentRepository.cs
+++ IStudentRepository.cs
@@ -5,8 +5,8 @@ namespace University{
   interface IStudentRepository{
     IEnumerable<Student> SelectAll();
     Student SelectByID(int id);
-    void Insert(Student);
-    void Update(Student);
+    void Insert(Student student);
+    void Update(Student student);
     void Delete(int id, int? replacementID);
     void Save();
   }
